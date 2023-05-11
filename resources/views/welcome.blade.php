b2183c25ad4897d931bbad7654696124d6d0a6fa
Anotn <antonrojkov_04@mail.ru>
2023-05-11 22:23:22 +0400

    сделал адаптив главной
    сделал адаптив главной

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,186 +15,140 @@
 </head>
 
 <body>
-    <!--блок шапка-->
-    <header>
+<!--блок шапка-->
+<header>
+    @php
+        use Illuminate\Support\Facades\Auth;
+        use App\Models\Cart;
+        if(Auth::user()){
+            $count_cart = Cart::where('user_id', Auth::user()->id)->get();
+            $count = 0;
+            for ($i=0; $i < count($count_cart); $i++)
+            {
+                $count +=$count_cart[$i]['quantity'];
+            }
+        }
+    @endphp
+    <div class="navbar">
+        <li class="logo"><a href="/" class="link-effect">Tea Grounds</a></li>
         <ul class="navigation">
-            <li><a href="" style="font-size:2.5vw;">Tea Grounds</a></li>
-            <li><a href="/about">О нас</a></li>
-            <li><a href="/shop">Магазин</a></li>
-            <li><a href="/blog">Блог</a></li>
-            <li><a href="/constructor">Конструктор</a></li>
-            <li><a href="/carts" class="link-effect">Корзина</a></li>
-            <li class="login"><a href="/register">
+            <li class="nav-item"><a href="/about" class="link-effect">О нас</a></li>
+            <li class="nav-item"><a href="/shop" class="link-effect">Магазин</a></li>
+            <li class="nav-item"><a href="/blog" class="link-effect">Блог</a></li>
+            <li class="nav-item"><a href="/constructor" class="link-effect">Конструктор</a></li>
+            <li class="nav-item"><a href="/carts" class="link-effect">
                     @auth
-                    {{ Auth::user()->name }}
+                        Корзина {{$count}}
                     @endauth
 
                     @guest
-                    Войти
+                        Корзина
                     @endguest
-                </a></li>
+                </a>
+            </li>
+            <li class="nav-item"><a href="/register">
+                    @auth
+                        {{ Auth::user()->name }}
+                    @endauth
+
+                    @guest
+                        Войти
+                    @endguest
+                </a>
+            </li>
         </ul>
-    </header>
+        <div class="hamburger">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    </div>
+</header>
 
-    <!--блок логотип-->
-    <section class="under_header">
-        <div class="logotip">
-            <img src="{{ asset("img/logotip.svg") }}" alt="">
-        </div>
-    </section>
+<!--блок логотип-->
+<section class="under_header">
+    <div class="logotip">
+        <img src="{{ asset("img/logotip.svg") }}" alt="">
+    </div>
+</section>
 
-    <!--блок О нас-->
-    <section class="about">
-        <h3>О нас</h3>
-        <div class="info">
-            <div class="text_one">
-                <p>
-                    Tea Grounds — был основан<br>
-                    на стремлении установить<br>
-                    стандарт качества и <br>
-                    расширить представление<br>
-                    о чае и его богатых,<br>
-                    вдохновляющих традициях. <br>
-                </p>
-                <img src="{{ asset("img/block1img1.png") }}" alt="">
-            </div>
-            <div class="separation"></div>
-            <div class="text_two">
-                <p>
-                    20 лет мы занимаемся чаем.<br>
-                    Из самых популярных <br>
-                    запросов мы составили <br>
-                    тематические подборки. <br>
-                    Ищите и находите нужный <br>
-                    чай для разных ситуаций.
-                </p>
-                <img src="{{ asset("img/block1img2.png") }}" alt="">
-            </div>
+<!--блок О нас-->
+<section class="about">
+    <div class="title1">
+        <p>TEA</p>
+    </div>
+    <h3>О нас</h3>
+    <div class="info">
+        <div class="text_one">
+            <p>
+                Tea Grounds — был основан
+                на стремлении установить
+                стандарт качества и
+                расширить представление
+                о чае и его богатых,
+                вдохновляющих традициях.
+            </p>
+            <img src="{{ asset("img/block1img1.png") }}" alt="">
         </div>
-        <div class="card-btn">
-            <button>Подробнее</button>
+        <div class="separation"></div>
+        <div class="text_two">
+            <p>
+                20 лет мы занимаемся чаем.
+                Из самых популярных
+                запросов мы составили
+                тематические подборки.
+                Ищите и находите нужный
+                чай для разных ситуаций.
+            </p>
+            <img src="{{ asset("img/block1img2.png") }}" alt="">
         </div>
-    </section>
+    </div>
+    <div class="title2">
+        <img src="{{ asset("img/svg1.svg") }}" alt="">
+        <p>GROUNDS</p>
+    </div>
+</section>
 
-    <!--блок ассортимент-->
-    <section class="assortment">
-        <h3>Ассортимент</h3>
-        <!--/*черный чай*/-->
-        <div class="black_tea">
-            <h4><span>—</span> Коллекция чернгого чая <span>—</span></h4>
-            <div class="subparagraph1">
-                <img src="{{ asset("img/svg5.svg") }}" alt="">
-            </div>
-            <div class="wrapper1">
-                <div class="item1">
-                    <img src="{{ asset("img/block2img1.png") }}" alt="">
-                    <p>Кимун</p>
+<!--блок каталог-->
+<section class="new">
+    @php
+        $randomTowars = App\Models\Towar::get()->random(4);
+    @endphp
+    <h3>Новые товары</h3>
+    <div class="wrappe2">
+        @foreach($randomTowars as $towars)
+            <div class="to1">
+                <div class="globa1">
+                    <img src="{{$towars->picture}}" alt="">
+                    <h5>{{$towars->name}}</h5>
+                    <p>{{$towars->price}} ₽/50гр.</p>
                 </div>
-                <div class="item2">
-                    <img src="{{ asset("img/block2img1.png") }}" alt="">
-                    <p>Эрл Грей</p>
-                </div>
-                <div class="item3">
-                    <img src="{{ asset("img/block2img1.png") }}" alt="">
-                    <p>Цейлонский</p>
-                </div>
-                <div class="item4">
-                    <img src="{{ asset("img/block2img1.png") }}" alt="">
-                    <p>Тонкая река</p>
-                </div>`
-            </div>
-            <div class="card-btn">
-                <button>Подробнее</button>
-            </div>
-        </div>
-        <!--/*зеленный чай*/-->
-        <div class="green_tea">
-            <h4><span>—</span> Коллекция зеленного чая <span>—</span></h4>
-            <div class="subparagraph2">
-                <img src="{{ asset("img/svg1.svg") }}" alt="">
-            </div>
-            <div class="wrapper2">
-                <div class="item1">
-                    <img src="{{ asset("img/block2img1.png") }}" alt="">
-                    <p>Ганпаудер</p>
-                </div>
-                <div class="item2">
-                    <img src="{{ asset("img/block2img1.png") }}" alt="">
-                    <p>Маофэн</p>
-                </div>
-                <div class="item3">
-                    <img src="{{ asset("img/block2img1.png") }}" alt="">
-                    <p>Сенча</p>
-                </div>
-                <div class="item4">
-                    <img src="{{ asset("img/block2img1.png") }}" alt="">
-                    <p>Матча</p>
+                <div class="btn_to1">
+                    <a href="{{route('OpenTovar', ['id'=> $towars->id])}}"><button class="btn_to1_more1">Подробнее</button></a>
+                    <form action="{{url('add_cart',$towars->id)}}" method="Post">
+                        @csrf
+                        <input class="in1" type="number" name="quantity" value="1" min="1">
+                        <button type="submit" class="btn_to1_more1">Добавить</button>
+                    </form>
                 </div>
             </div>
-            <div class="card-btn">
-                <button>Подробнее</button>
-            </div>
-        </div>
-        <!--/*Коллекция смешанного чая*/-->
-        <div class="fusion_tea">
-            <h4><span>—</span> Коллекция смешанного чая <span>—</span></h4>
-            <div class="subparagraph3">
-                <img src="{{ asset("img/svg6.svg") }}" alt="">
-            </div>
-            <div class="wrapper3">
-                <div class="item1">
-                    <img src="{{ asset("img/block2img1.png") }}" alt="">
-                    <p>Витаминный</p>
-                </div>
-                <div class="item2">
-                    <img src="{{ asset("img/block2img1.png") }}" alt="">
-                    <p>Шанти-Йога</p>
-                </div>
-                <div class="item3">
-                    <img src="{{ asset("img/block2img1.png") }}" alt="">
-                    <p>Ку цяо хей</p>
-                </div>
-                <div class="item4">
-                    <img src="{{ asset("img/block2img1.png") }}" alt="">
-                    <p>Лао Сун</p>
-                </div>
-            </div>
-            <div class="card-btn">
-                <button>Подробнее</button>
-            </div>
-        </div>
-        <!--/*конструктор*/-->
-        <div class="konstructor">
-            <div class="title1">
-                <p>TEA</p>
-            </div>
-            <div class="konstructor_info">
-                <p>
-                    СОЗДАЙ СВОЙ СОБСТВЕННЫЙ <br>
-                    <br>
-                    И ВКУСНЫЙ ЧАЙ С ШАРИКАМИ
-                </p>
-                <img src="{{ asset("img/block2img2.png") }}" alt="">
-            </div>
-            <div class="title2">
-                <p>GROUNDS</p>
-            </div>
-        </div>
-    </section>
+        @endforeach
+    </div>
+</section>
 
-    <!--блок Блог-->
-    <section class="blog">
-        <h3>Статьи</h3>
-        @php
+<!--блок Блог-->
+<section class="news_more">
+    @php
         $randomNewses = App\Models\News::get()->random(4);
-        @endphp
-        <div class="container">
-            @foreach($randomNewses as $el)
+    @endphp
+    <h3>Статьи</h3>
+    <div class="container">
+        @foreach($randomNewses as $el)
             <div class="card">
                 <img src="{{$el->picture}}" alt="">
                 <div class="card-content">
                     <div class="card-title">
-                        <p>{{$el->paragraph->name}}</p>
+                        <h4>{{$el->paragraph->name}}</h4>
                     </div>
                     <p class="card-text">
                         {{$el->subject}}
@@ -204,79 +158,95 @@
                     </div>
                 </div>
             </div>
-            @endforeach
-        </div>
-        <div class="card-btn">
-            <button>Подробнее</button>
-        </div>
-    </section>
+        @endforeach
+    </div>
+    <a href="#" class="go-top"><img src="{{asset('img/upbtn.png')}}" alt=""></a>
+</section>
 
-    <!--блок Отзывы-->
-    <section class="reviews">
-        <h3>Отзывы</h3>
-        <div class="container2">
-            @foreach($randomReviews as $it)
-            <div class="item1_rev">
+<!--блок Отзывы-->
+<section class="reviews">
+    <h3>Отзывы</h3>
+    <div class="container2">
+        @foreach($randomReviews as $it)
+            <div class="rev">
                 <h5>{{$it->fio}}</h5>
                 <p>{{$it->site_review}}</p>
             </div>
-            @endforeach
-        </div>
-    </section>
+        @endforeach
+    </div>
+</section>
 
-    <!--блок Напишите нам-->
-    <section class="napishite">
-        <h3>Напишите нам</h3>
-        <div class="container3">
-            <div class="block_info">
-                <div class="text1">
-                    <h6>Есть вопросы?</h6>
-                    <p>Если возникают какие либо технические проблемы, у вас есть рекомендации к заявкам,
-                        или же вы хотите получить рекомендации, то оставьте свои данные и мы обязательно свяжемся с вами.
-                    </p>
-                </div>
-                <div class="text2">
-                    <h6>Адрес:</h6>
-                    <p>ул. Богдана Хмельницкого, 39, Астрахань, Астраханская обл., 414024</p>
-                </div>
-                <div class="text3">
-                    <h6>Контактный телефон:</h6>
-                    <p>+79029783511</p>
-                </div>
-                <div class="text4">
-                    <h6>E-mail:</h6>
-                    <p>ruteagrounds@gmail.com</p>
-                </div>
+<!--блок Напишите нам-->
+<section class="napishite">
+    <h3>Напишите нам</h3>
+    <div class="container3">
+        <div class="block_info">
+            <div class="text">
+                <h6>Есть вопросы?</h6>
+                <p>Если возникают какие либо технические проблемы, у вас есть рекомендации к заявкам,
+                    или же вы хотите получить рекомендации, то оставьте свои данные и мы обязательно свяжемся с вами.
+                </p>
             </div>
-            <form name="{{route('welcome')}}" method="Post">
-                @csrf
-                <input name="name" type="text" placeholder="Введите имя">
-                <br>
-                <input name="email" type="text" placeholder="Введите почту">
-                <br>
-                <input size="30" name="header" type="text" placeholder="Введите название темы" />
-                <br>
-                <textarea name="message" placeholder="Введите сообщение" cols="40" rows="15"></textarea>
-                <br>
-                <button type="submit" class="btn_top1">Отправить</button>
-            </form>
+            <div class="text">
+                <h6>Адрес:</h6>
+                <p>ул. Богдана Хмельницкого, 39, Астрахань, Астраханская обл., 414024</p>
+            </div>
+            <div class="text">
+                <h6>Контактный телефон:</h6>
+                <p>+79029783511</p>
+            </div>
+            <div class="text">
+                <h6>E-mail:</h6>
+                <p>ruteagrounds@gmail.com</p>
+            </div>
         </div>
-    </section>
+        <form name="{{route('welcome')}}" method="Post">
+            @csrf
+            <input name="name" type="text" placeholder="Введите имя">
+            <br>
+            <input name="email" type="text" placeholder="Введите почту">
+            <br>
+            <input size="30" name="header" type="text" placeholder="Введите название темы" />
+            <br>
+            <textarea name="message" placeholder="Введите сообщение" cols="40" rows="15"></textarea>
+            <br>
+            <button type="submit" class="btn_top1">Отправить</button>
+        </form>
+    </div>
+    <a href="#" class="go-top"><img src="img/upbtn.png" alt=""></a>
+</section>
 
-    <!--блок footer-->
-    <footer>
-        <ul class="navigation_footer">
-            <li class="logo"><a href="/" class="link-effect">Tea Grounds</a></li>
+<!--блок footer-->
+<footer>
+    <nav class="navbar">
+        <li class="logo"><a href="/" class="link-effect">Tea Grounds</a></li>
+        <ul class="navigation">
             <li><a href="/about" class="link-effect">О нас</a></li>
             <li><a href="/shop" class="link-effect">Магазин</a></li>
             <li><a href="/blog" class="link-effect">Блог</a></li>
             <li><a href="/constructor" class="link-effect">Конструктор</a></li>
+            <li><a href="/carts" class="link-effect">Корзина</a></li>
+            <li><a href="/register" class="link-effect">
+                    @auth
+                        {{ Auth::user()->name }}
+                    @endauth
+
+                    @guest
+                        Войти
+                    @endguest
+                </a></li>
         </ul>
-        <a href="" style="font-size:1.2vw; color: rgba(255, 255, 255, 0.2);">Политика конфидециальности</a>
-        <div class="cop">
-            <p>Tea Grounds © 2022 Все права защищены</p>
-        </div>
-    </footer>
+    </nav>
+    <div class="conf">
+        <a href="">Политика конфидециальности</a>
+    </div>
+    <div class="cop">
+        <p>Tea Grounds © 2022 Все права защищены</p>
+    </div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="js/buttonUp.js"></script>
+<script src="js/burgerMenu.js"></script>
 </body>
 
 </html>
