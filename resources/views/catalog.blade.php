ac0d7c83282a8c35201e3eb082550a49ccf43992
Anotn <antonrojkov_04@mail.ru>
2023-05-11 18:35:45 +0400

    сделал адаптив каталог
    сделал адаптив каталог

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -64,116 +64,70 @@
         </div>
     </div>
 </header>
-
-    <section class="paragraph">
-        <h3>{{$category->name}}</h3>
-    </section>
-    <!--блок товары-->
-{{--    <section class="tovar">--}}
-{{--        <div class="container6">--}}
-{{--            <div class="filtr">--}}
-{{--                <h6>Фильтры</h6>--}}
-{{--            </div>--}}
-{{--            <div class="country">--}}
-{{--                <h6>Страна</h6>--}}
-{{--                <form class="country_form">--}}
-{{--                    <label>--}}
-{{--                        <input type="checkbox" name="languages" value="HTML">--}}
-{{--                        Китай--}}
-{{--                    </label>--}}
-{{--                    <br>--}}
-{{--                    <label>--}}
-{{--                        <input type="checkbox" name="languages" value="CSS">--}}
-{{--                        Индия--}}
-{{--                    </label>--}}
-{{--                    <br>--}}
-{{--                    <label>--}}
-{{--                        <input type="checkbox" name="languages" value="JS">--}}
-{{--                        Япония--}}
-{{--                    </label>--}}
-{{--                    <br>--}}
-{{--                    <label>--}}
-{{--                        <input type="checkbox" name="languages" value="JS">--}}
-{{--                        Россия--}}
-{{--                    </label>--}}
-{{--                    <br>--}}
-{{--                    <label>--}}
-{{--                        <input type="checkbox" name="languages" value="JS">--}}
-{{--                        Кения--}}
-{{--                    </label>--}}
-{{--                </form>--}}
-{{--            </div>--}}
-{{--            <div class="price">--}}
-{{--                <h6>Цена</h6>--}}
-{{--                <form class="price_form">--}}
-{{--                    <label>--}}
-{{--                        <input type="checkbox" name="languages" value="HTML">--}}
-{{--                        Дорогой чай--}}
-{{--                    </label>--}}
-{{--                    <br>--}}
-{{--                    <label>--}}
-{{--                        <input type="checkbox" name="languages" value="CSS">--}}
-{{--                        Дешевый чай--}}
-{{--                    </label>--}}
-{{--                    <br>--}}
-{{--                </form>--}}
-{{--            </div>--}}
-{{--            <div class="btn1">--}}
-{{--                <button class="btn_more1">Применить</button>--}}
-{{--            </div>--}}
-{{--            <div class="btn2">--}}
-{{--                <button class="btn_more2">Сбросить</button>--}}
-{{--            </div>--}}
-{{--            --}}{{-- <div class="teg">--}}
-{{--            --}}{{-- <p>Tea Grounds</p>--}}
-{{--            --}}{{-- </div>--}}
-{{--        </div>--}}
-{{--        <div class="separation"></div>--}}
-    <h4><span>—</span> Все товары <span>—</span></h4>
-    <div class="container7">
+{{----}}
+<section class="paragraph">
+    <h3>{{$category->name}}</h3>
+</section>
+<!--блок товары-->
+<section class="towar">
+    <h3><span>—</span> Все товары <span>—</span></h3>
+    <div class="title1">
+        <img src="{{ asset("img/svg1.svg") }}" alt="">
+        <img src="{{ asset("img/svg5.svg") }}" alt="">
+    </div>
+    <div class="wrapper3">
+        <div class="container7">
             @foreach($towars as $towars)
-            <div class="top1">
-                <div class="global1">
-                    <img src="{{$towars->picture}}" alt="">
-                    <h5>{{$towars->name}}</h5>
-                    <p>{{$towars->price}} ₽/50гр.</p>
+                <div class="top1">
+                    <div class="global1">
+                        <img src="{{$towars->picture}}" alt="">
+                        <h5>{{$towars->name}}</h5>
+                        <p>{{$towars->price}} ₽/50гр.</p>
+                    </div>
+                    <div class="btn_top1">
+                        <a href="{{route('OpenTovar', ['id'=> $towars->id])}}"><button class="btn_top1_more1">Подробнее</button></a>
+                        <form action="{{url('add_cart',$towars->id)}}" method="Post">
+                            @csrf
+                            <input type="number" name="quantity" value="1" min="1">
+                            <button type="submit" class="btn_top1_more1">Добавить</button>
+                        </form>
+                    </div>
                 </div>
-                <div class="btn_top1">
-                    <a href="{{route('OpenTovar', ['id'=> $towars->id])}}"><button class="btn_top1_more1">Подробнее</button></a>
-                    <form action="{{url('add_cart',$towars->id)}}" method="Post">
-                        @csrf
-                        <input type="number" name="quantity" value="1" min="1">
-                        <button type="submit" class="btn_top1_more1">Добавить</button>
-                    </form>
-                </div>
-            </div>
             @endforeach
-            {{-- <div class="pagination">--}}
-            {{-- <a href="#">&laquo;</a>--}}
-            {{-- <a class="active" href="#">1</a>--}}
-            {{-- <a href="#">2</a>--}}
-            {{-- <a href="#">3</a>--}}
-            {{-- <a href="#">4</a>--}}
-            {{-- <a href="#">5</a>--}}
-            {{-- <a href="#">6</a>--}}
-            {{-- <a href="#">&raquo;</a>--}}
-            {{-- </div>--}}
         </div>
-    </section>
-
-    <!--блок footer-->
-    <footer>
-        <ul class="navigation_footer">
-            <li class="logo"><a href="/" class="link-effect">Tea Grounds</a></li>
+    </div>
+    <a href="#" class="go-top"><img src="img/upbtn.png" alt=""></a>
+</section>
+<!--блок footer-->
+<footer>
+    <nav class="navbar">
+        <li class="logo"><a href="/" class="link-effect">Tea Grounds</a></li>
+        <ul class="navigation">
             <li><a href="/about" class="link-effect">О нас</a></li>
             <li><a href="/shop" class="link-effect">Магазин</a></li>
             <li><a href="/blog" class="link-effect">Блог</a></li>
             <li><a href="/constructor" class="link-effect">Конструктор</a></li>
+            <li><a href="/carts" class="link-effect">Корзина</a></li>
+            <li><a href="/register" class="link-effect">
+                    @auth
+                        {{ Auth::user()->name }}
+                    @endauth
+
+                    @guest
+                        Войти
+                    @endguest
+                </a></li>
         </ul>
-        <a href="" style="font-size:1.2vw; color: rgba(255, 255, 255, 0.2);">Политика конфидециальности</a>
-        <div class="cop">
-            <p>Tea Grounds © 2022 Все права защищены</p>
-        </div>
-    </footer>
+    </nav>
+    <div class="conf">
+        <a href="">Политика конфидециальности</a>
+    </div>
+    <div class="cop">
+        <p>Tea Grounds © 2022 Все права защищены</p>
+    </div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
+<script src="{{asset('js/buttonUp.js')}}"></script>
+<script src="{{asset('js/burgerMenu.js')}}"></script>
 </body>
 </html>
